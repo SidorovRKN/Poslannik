77555ecc5248b4b54a82a25159b34656ec58b1a7
Sidorov Nicolay <92115886+SidorovRKN@users.noreply.github.com>
2024-01-22 18:02:13 +0300

    Update settings.py

--- Poslannik_auto/Poslannik_auto/settings.py
+++ Poslannik_auto/Poslannik_auto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-fqfp0@l2b94*-!k_p7chcpq(6s%dgjra+-z!za%x3z2zg!-h'
+SECRET_KEY = 'django-insecure-6-pipipupu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
